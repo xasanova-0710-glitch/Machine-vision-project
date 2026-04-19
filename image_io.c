3db21641cc498eb3776b8ecdecdbb3d23450480e
xasanova-0710-glitch <xasanova-0710@mail.ru>
2026-04-19 19:54:07 +0700

    Update image_io.c

--- image_io.c
+++ image_io.c
@@ -27,6 +27,56 @@ Image* load_image(const char* filename) {
     return data_true;
 }
 
+int save_image(const char* filename, Image* img) {
+    if (img == NULL || img->bytes == NULL) {
+        return 0; 
+    }
+
+    int result = stbi_write_png(filename, img->width, img->height, img->channels, img->bites, img->width * img->channels);
+    
+    return result; // если получится, то 1, если нет, то 0
+}
+
+Image* create_image(int w, int h, int chan) {
+    Image* img = malloc(sizeof(Image));
+    if (img == NULL) {
+        return NULL;
+    }
+
+    img->width = w;
+    img->height = h;
+    img->channels = chan;
+
+    img->bytes = malloc(w * h * chan);
+
+    if (img->bytes == NULL) {
+        free(img);
+        return NULL;
+    }
+
+    return img;
+}
+
+Image* convert_to_grayscale(const Image* img) {
+    if (img == NULL || img->bytes == NULL) return NULL;
+
+    Image* gray_img = create_image(img->width, img->height, 1);
+    if (gray_img == NULL) return NULL;
+
+    for (int i = 0; i < img->width * img->height; i++) {
+        int idx = i * img->channels;
+
+        unsigned char r = img->bytes[idx];
+        unsigned char g = img->bytes[idx + 1];
+        unsigned char b = img->bytes[idx + 2];
+
+        unsigned char gray_value = (unsigned char)(0.299f * r + 0.587f * g + 0.114f * b);
+
+        gray_img->bytes[i] = gray_value;
+    }
+
+    return gray_img;
+}
 void free_image(Image* img) {
     if (img == NULL) {
         return;
