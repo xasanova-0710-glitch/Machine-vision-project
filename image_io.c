2258a3ca44899c14983eef2ec5d0309db74bd844
xasanova-0710-glitch <xasanova-0710@mail.ru>
2026-05-24 17:40:56 +0700

    image_io.c

--- image_io.c
+++ image_io.c
@@ -1,10 +1,14 @@
 #include "image_io.h"
 #include <stdlib.h>
+#include <string.h>
 
 #define STB_IMAGE_IMPLEMENTATION //преобразует stb_image.h в проргаммный код из хэдера, только в одном сишном файле, иначе ошибка
 #include "stb_image.h"
 
-Image* load_image(const char* filename) {
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+Image* load_image(const char* filename) { //мы загружаем изображение
     int w, h, chan;
     unsigned char* data = stbi_load(filename, &w, &h, &chan, 0); //загружаем нужные данные, 0 - использовать исходное количество каналов
 
@@ -28,10 +32,15 @@ Image* load_image(const char* filename) {
 }
 
 int save_image(const char* filename, Image* img) {
-    if (img == NULL || img->bites == NULL) {
+    if (img == NULL || img->bites == NULL || filename == NULL) {
         return 0; 
     }
 
+    if (strstr(filename, ".jpg") || strstr(filename, ".JPG") || 
+        strstr(filename, ".jpeg") || strstr(filename, ".JPEG")) {
+        return stbi_write_jpg(filename, img->width, img->height, img->channels, img->bites, 100);
+    }
+    
     int result = stbi_write_png(filename, img->width, img->height, img->channels, img->bites, img->width * img->channels);
     
     return result; // если получится, то 1, если нет, то 0
@@ -57,11 +66,16 @@ Image* create_image(int w, int h, int chan) {
     return img;
 }
 
-Image* convert_to_grayscale(const Image* img) {
+Image* convert_to_grayscale(const Image* img) { //конвертируем в серый
     if (img == NULL || img->bites == NULL) return NULL;
 
     Image* gray_img = create_image(img->width, img->height, 1);
     if (gray_img == NULL) return NULL;
+
+    if (img->channels == 1) {
+        memcpy(gray_img->bites, img->bites, img->width * img->height);
+        return gray_img;
+    }
 
     for (int i = 0; i < img->width * img->height; i++) {
         int idx = i * img->channels;
@@ -77,13 +91,14 @@ Image* convert_to_grayscale(const Image* img) {
 
     return gray_img;
 }
-void free_image(Image* img) {
+
+void free_image(Image* img) { //удаляем изображение
     if (img == NULL) {
         return;
     }
 
     if (img->bites != NULL) {
-    stbi_image_free(img->bites); //удаление массива пикселей
+        free(img->bites); //удаление массива пикселей
     }
 
     free(img);
